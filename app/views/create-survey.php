f71921e3afde427520c4a01f8abd9e84a7bbdb6a
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-06-25 23:25:14 +0300

    ...

--- app/views/create-survey.php
+++ app/views/create-survey.php
@@ -66,7 +66,7 @@
                     <span>Questions</span>
 
                     <div x-data="{ isOpen: false }" class="relative inline-block">
-                        <a href="javascript:void(0)" @click="isOpen = !isOpen" class="flex rounded-md bg-gray-700 px-2 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 dark:hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+                        <a href="javascript:void(0)" @click="isOpen = !isOpen" class="flex rounded-md bg-gray-700 p-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 dark:hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                             </svg>
