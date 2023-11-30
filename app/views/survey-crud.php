b8d8ec5f30f5368a9cacdc875c2fd71aaa4f53e9
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-11-30 14:37:56 +0300

    :(((

--- app/views/survey-crud.php
+++ app/views/survey-crud.php
@@ -75,7 +75,7 @@
                     <div class="col-span-full">
                         <label for="about" class="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">About</label>
                         <div class="mt-2">
-                            <textarea tinymce="true" id="about" name="about" class="px-4 block w-full rounded-md border-0 outline-none py-1.5 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:bg-slate-700 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"></textarea>
+                            <textarea id="about" name="about" class="px-4 block w-full rounded-md border-0 outline-none py-1.5 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:bg-slate-700 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"></textarea>
                         </div>
                         <p class="mt-3 text-sm leading-6 text-gray-600 dark:text-gray-300">Write a few sentences about the survey.</p>
                     </div>
