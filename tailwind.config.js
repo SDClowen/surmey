d8492ee088103f3bbb8b7570a972d19fb9350e9e
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-12-13 11:30:49 +0300

    fix tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{html,php}", "./public/**/*.js"],
+  safelist: [
+    'alert-info',
+    'alert-warning',
+    'alert-success',
+    'alert-danger' 
+  ],
   plugins: [],
   theme: {
     fontFamily: {
