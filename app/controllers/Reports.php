266b902ce79e3e02281dac3fe9b6278bcdac96e6
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2024-01-03 16:49:44 +0300

    ....

--- app/controllers/Reports.php
+++ app/controllers/Reports.php
@@ -9,78 +9,6 @@ use Core\Attributes\route;
 class Reports extends Controller
 {
     #[route(method: route::get | route::xhr_get, session: "user", otherwise: "/auth")]
-    public function watch2(int $surveyId)
-    {
-        #$survey = Survey::existsByUserId(User::id(), "id", $surveyId);
-        $survey = Survey::exists("id", $surveyId);
-        if (!$survey)
-            redirect();
-
-        $answerData = $this->db->select("answers.*, personals.fullname, personals.department")
-            ->from("answers")
-            ->join("personals", "personals.id = answers.personalId")
-            ->where("surveyId", "=", $surveyId)
-            ->results();
-
-        /*echo "<pre>";
-        print_r($answerData);*/
-
-        $questionData = json_decode($survey->data);
-
-        foreach ($questionData as $key => $value) {
-
-            if($value->type == "description")
-                continue;
-
-            $search = function($k) use ($value, $answerData){
-                return array_filter($answerData, function($aw_V, $aw_K) use ($value, $k){
-
-                    $decodedJson = json_decode($aw_V->data, JSON_OBJECT_AS_ARRAY);
-                    if(!$decodedJson)
-                        return;
-
-                    $s = $value->type == "checkbox" ? $value->slug.$k : $value->slug;
-
-                    $exists = array_key_exists($s, $decodedJson); 
-                    if($exists && $value->type == "radio")
-                        return $decodedJson[$s] == $k;
-
-                    return $exists;
-
-                }, ARRAY_FILTER_USE_BOTH);
-            };
-            
-            echo "<b>".$value->title."</b><br>";
-            echo "***************************************";
-            
-            foreach ($value->answers as $k => $answer) {
-
-                $filtered = $search($k);
-                    
-                $slug = ($value->type == "checkbox" ? $value->slug.$k : $value->slug);
-
-                $count = count($filtered);
-                echo "<br><i>$k [ $slug ] - $answer (<b>$count</b>)</i>";
-            }
-
-            if($value->type == "textarea")
-            {
-                $data = $search(0);
-                foreach($data as $v)
-                {
-                    $d = json_decode($v->data, JSON_OBJECT_AS_ARRAY)[$value->slug];
-                    if(empty($d))
-                        continue;
-                    
-                    echo $d."<br>";
-                }
-            }
-
-            echo "<hr>";
-        }
-    }
-
-    #[route(method: route::get | route::xhr_get, session: "user", otherwise: "/auth")]
     public function watch(int $surveyId)
     {
         #$survey = Survey::existsByUserId(User::id(), "id", $surveyId);
@@ -97,7 +25,7 @@ class Reports extends Controller
         ]);
     }
 
-    #[route(uri: "csv", method: route::xhr_get)]
+    #[route(method: route::get)]
     public function csv(int $surveyId)
     {
         $survey = Survey::existsByUserId(User::id(), "id", $surveyId);
--- app/views/reports.php
+++ app/views/reports.php
@@ -15,7 +15,7 @@
                 <span class="ml-1">Grafik</span>
             </a>
         </div>
-        <div class="relative inline-flex">
+        <div class="relative inline-flex hidden">
             <button id="dropdownMenuIconButton" @click="dropIsOpen = !dropIsOpen" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-600 bg-gray-100 hover:bg-gray-200 rounded-lg focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
                     <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
@@ -26,7 +26,7 @@
             <div id="dropdownRadioHelper" x-show="dropIsOpen" @click.away="dropIsOpen = false" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-90" class="absolute right-0 top-8 py-2 mt-2 overflow-hidden origin-top z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-60 border dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioHelperButton">
                     <li>
-                        <a data-url="/reports/csv/{$surveyId}" target="blank" class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                        <a href="/reports/csv/{$surveyId}" target="blank" class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                             <div class="flex items-center h-5">
                                 <svg class="w-8 h-8 align-middle" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
