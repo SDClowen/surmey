0cbc12e393c9da20c8051c1b56448ebed7f92d64
SDClowen <mahmutyildirim@outlook.com.tr>
2025-03-24 10:42:02 +0300

    fixed pantest

--- app/config/app.php
+++ app/config/app.php
@@ -3,5 +3,5 @@ return [
     "latte" => true,
     "locale" => "tr_TR.utf8",
     "timezone" => "Europe/Istanbul",
-    "mode" => "development" #Development or Production
+    "mode" => "Production" #Development or Production
 ];
--- app/controllers/settings.php
+++ app/controllers/settings.php
@@ -7,7 +7,7 @@ use Core\Attributes\route;
 
 class Settings extends Controller
 {
-    #[route(method: route::get | route::xhr_get)]
+    #[route(method: route::get | route::xhr_get, session: "user", otherwise: "/auth")]
     public function index()
     {
         $this->view("main", "settings", lang("settings"), ["user" => User::info()]);
