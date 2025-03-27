ab578c9fcbdd361bff3dfcb1e43368d3f7ed3a22
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2025-03-27 18:02:22 +0300

    Update Participator.php

--- app/models/Participator.php
+++ app/models/Participator.php
@@ -21,7 +21,7 @@ final class Participator extends Model
             ->from("answers")
             ->where("surveyId", "=", $surveyId)
             ->where("personalId", "=", $personalId)
-            ->where("done", "=", $isDone)
+            ->where("done", "=", (int)$isDone)
             ->first();
     }
 
